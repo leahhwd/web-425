7f715f7583744de506258f6b797ce8af3aad6e8e
leahmkh <105560625+leahmkh@users.noreply.github.com>
2024-09-08 13:40:33 -1000

    week 4

--- rpg-character-builder/src/app/create-character/create-character.component.spec.ts
+++ rpg-character-builder/src/app/create-character/create-character.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
 
 import { CreateCharacterComponent } from './create-character.component';
 
@@ -20,4 +20,60 @@ describe('CreateCharacterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should generate a random character ID between 1 and 1000 with no decimal places', () => {
+
+    component.name = "Harry Viking";
+    component.gender = "other";
+    component.class = "warrior";
+
+    const test1 = {
+      value: {
+       name: component.name,
+       gender: component.gender,
+       class: component.class
+      },
+      valid: true
+    }
+    component.onSubmit(test1);
+    const addedCharacter = component.characters[0];
+
+    expect(addedCharacter.id).toBeGreaterThan(0);
+    expect(addedCharacter.id).toBeLessThanOrEqual(1000);
+    expect(Number.isInteger(addedCharacter.id)).toBe(true);
+  });
+
+  it('should add a character with correct customization', () => {
+    component.name = "Sana Bunderwunt";
+    component.gender = "male";
+    component.class = "rogue";
+
+    const test2 = {
+      value: {
+       name: "Sana Bunderwunt",
+       gender: "male",
+       class: "rogue",
+      },
+      valid: true
+    }
+    component.onSubmit(test2);
+    const addedCharacter = component.characters[0];
+
+    expect(addedCharacter.name).toBe("Sana Bunderwunt");
+    expect(addedCharacter.gender).toBe("male");
+    expect(addedCharacter.class).toBe("rogue");
+  });
+
+  it('should reset all form fields to default value after resetForm is called', () => {
+
+    component.name = "Kelley Widle";
+    component.gender = "female";
+    component.class = "warrior";
+
+    component.resetForm();
+
+    expect(component.name).toBe(" ");
+    expect(component.class).toBe(" ");
+    expect(component.gender).toBe(" ");
+  })
 });
--- rpg-character-builder/src/app/create-character/create-character.component.ts
+++ rpg-character-builder/src/app/create-character/create-character.component.ts
@@ -1,16 +1,155 @@
+export interface Character {
+  id: number;
+  name: string;
+  gender: string;
+  class: string;
+}
+
+
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-character',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   template: `
-    <p>
-      create-character works!
-    </p>
+    <div class="characters-form-container">
+     <form class="character-form" #characterCreationForm = "ngForm" (ngSubmit)="onSubmit(characterCreationForm)" (reset)="resetForm()">
+      <h1>Create Your Own Character!</h1>
+
+      <fieldset>
+       <legend>My Character</legend>
+       <label for="name">Name</label>
+       <input type="text" id="name" name="name" class="name-input" [(ngModel)]="name" ngModel>
+
+
+       <div>
+        <label for="gender">Gender</label>
+        <select name="gender" id="gender" [(ngModel)]="gender" ngModel>
+         <option value="male">Male</option>
+         <option value="female">Female</option>
+         <option value="other">Other</option>
+        </select>
+       </div>
+
+       <div>
+        <label for="class">Class</label>
+        <select name="class" id="class" [(ngModel)]="class" ngModel>
+         <option value="warrior">Warrior</option>
+         <option value="mage">Mage</option>
+         <option value="rogue">Rogue</option>
+        </select>
+       </div>
+
+       <input type="submit" value="Create Character"/>
+      </fieldset>
+     </form>
+
+     <div class="created-characters">
+      <h1>Characters:</h1>
+      @if(characters.length > 0){
+       <ul>
+        @for(characters of characters; track characters){
+         <li>
+          <strong>Name: </strong> {{ characters.name }}
+          <strong>Gender: </strong> {{ characters.gender }}
+          <strong>Class: </strong> {{ characters.class }}
+         </li>
+        }
+       </ul>
+      }
+     </div>
+    </div>
+
   `,
-  styles: ``
+  styles: [
+    `
+    .characters-form-container {
+      display: flex;
+      justify-content: space-between;
+      gap: 10px;
+    }
+
+    .character-form {
+      flex: 1;
+      margin-right: 20px;
+    }
+
+    .created-characters {
+      flex: 1;
+    }
+
+    fieldset {
+      margin-bottom: 20px;
+    }
+
+    label, select, name-input {
+      display: block;
+      margin-bottom: 5px;
+    }
+
+    select {
+      width: 100%;
+    }
+
+    .name-input {
+      width: 100px;
+    }
+
+    input[type="submit"] {
+      float: right;
+    }
+
+    .create-characters li {
+      margin-bottom: 10px;
+      padding: 5px;
+    }
+    `
+  ]
 })
 export class CreateCharacterComponent {
+ characters: Character[];
+ characterId: number;
+ name: string = '';
+ gender: string = '';
+ class: string = '';
 
+
+  constructor() {
+   this.characters = [];
+   this.characterId = 0;
+   this.name = '';
+   this.gender = '';
+   this.class = '';
+  }
+
+
+
+  onSubmit(form: any) {
+   console.log('Form value:', form.value);
+
+   if(form.valid) {
+
+     this.characterId = Math.floor(Math.random() * 1000) +1;
+
+     const newCharacter = {
+       id: this.characterId,
+       name: form.value.name,
+       gender: form.value.gender,
+       class: form.value.class
+     };
+
+     this.characters.push(newCharacter);
+     this.resetForm();
+   } else {
+     console.error('Character not created')
+   }
+ }
+ resetForm () {
+  this.name = " ";
+  this.gender = " ";
+  this.class = " ";
+ }
 }
