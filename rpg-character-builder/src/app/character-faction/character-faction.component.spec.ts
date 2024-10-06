b1fa37520c8265ddba065639fd0673a157a2f113
leahmkh <105560625+leahmkh@users.noreply.github.com>
2024-10-06 12:34:58 -1000

    week 8

--- rpg-character-builder/src/app/character-faction/character-faction.component.spec.ts
+++ rpg-character-builder/src/app/character-faction/character-faction.component.spec.ts
@@ -1,16 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CharacterFactionComponent } from './character-faction.component';
 
 describe('CharacterFactionComponent', () => {
   let component: CharacterFactionComponent;
   let fixture: ComponentFixture<CharacterFactionComponent>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CharacterFactionComponent]
+      imports: [CharacterFactionComponent, HttpClientTestingModule]
     })
     .compileComponents();
+
+    httpTestingController = TestBed.inject(HttpTestingController);
 
     fixture = TestBed.createComponent(CharacterFactionComponent);
     component = fixture.componentInstance;
@@ -20,4 +23,37 @@ describe('CharacterFactionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should handle errors when data is not found', () => {
+    const expectedMessage = 'No character factions found.'
+    const req = httpTestingController.expectOne(`http://localhost:3000/api/character-factions`);
+    req.flush('Factions not found', {status: 404, statusText: 'Not Found'});
+
+    expect(component.noFactionMessage).toEqual(expectedMessage);
+  })
+
+  it('should correctly fetch a list of character factions', () => {
+    const mockFactions = [
+      {name: 'The Band of Rogues', description: 'The Band of Rogues is a faction full of independent rogues.'},
+      {name: 'The Amazing Mages', description: 'A faction full of amazing mages.'}
+    ];
+
+    const req = httpTestingController.expectOne(`http://localhost:3000/api/character-factions`);
+    req.flush(mockFactions);
+
+    expect(component.factions).toEqual(mockFactions);
+  })
+
+  it('should update the characterFactions div when character factions are found.', () => {
+
+    const mockFaction = {
+      name: 'The Band of Rogues',
+      description: 'The Band of Rogues is a faction full of independent rogues.'
+    }
+
+    const req = httpTestingController.expectOne(`http://localhost:3000/api/character-factions`);
+    req.flush(mockFaction);
+
+    expect(component.factions).toEqual(mockFaction);
+  })
 });
--- rpg-character-builder/src/app/character-faction/character-faction.component.ts
+++ rpg-character-builder/src/app/character-faction/character-faction.component.ts
@@ -1,16 +1,52 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-character-faction',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      character-faction works!
-    </p>
+    @if(factions) {
+      <div class="faction-container">
+       <h1>Name: {{ factions.name }}</h1>
+       <p>Description: {{ factions.description }} </p>
+      </div>
+    } @else {
+      <div class="faction-container">
+       <h1> {{ noFactionMessage }} </h1>
+      </div>
+    }
   `,
-  styles: ``
+  styles: `
+
+  .faction-container {
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+  }
+  `
 })
 export class CharacterFactionComponent {
+factions: any = null;
+noFactionMessage: string ='';
 
+constructor(private http: HttpClient) {
+
+  this.http.get(`http://localhost:3000/api/character-factions`).subscribe({
+    next: (res) => {
+      console.log(res);
+      this.factions = res;
+    },
+
+    error: (err) => {
+      console.error('Error fetching factions', err);
+      if(err.error === 'Faction not found') {
+        this.noFactionMessage = 'No character factions found.';
+      } else {
+        this.noFactionMessage = 'No character factions found.';
+      }
+    }
+  });
+}
 }
